e6973f1862b6c8faebf5d87bbbc74c96f05e3388
alexanderrolf <alexanderolflifestyle@gmail.com>
2025-09-30 20:23:37 +0200

    First push

---
+++ src/main/java/com/example/kalenderbackend/KalenderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kalenderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalenderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalenderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kalenderbackend/KalenderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kalenderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalenderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
