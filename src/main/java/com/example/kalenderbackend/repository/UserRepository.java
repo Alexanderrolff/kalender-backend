99ae60cc17c4a20357bbeff6c5956838d6e1b91d
alexanderrolf <alexanderolflifestyle@gmail.com>
2025-10-01 11:48:25 +0200

    Add security

--- src/main/java/com/example/kalenderbackend/repository/UserRepository.java
+++ src/main/java/com/example/kalenderbackend/repository/UserRepository.java
@@ -3,10 +3,14 @@ package com.example.kalenderbackend.repository;
 import com.example.kalenderbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
+
